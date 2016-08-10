e5fdd65a47835105d8a32e60907b3c93715eb5e9
sloanibe <sloanibe@gmail.com>
2016-08-10 10:45:11 -0700

    test2

--- src/test/java/monk/cashflo/HelloWorldTest.java
+++ src/test/java/monk/cashflo/HelloWorldTest.java
@@ -12,7 +12,7 @@ public class HelloWorldTest {
     @Before
     public void setUp() throws Exception {
         helloWorld = new HelloWorld();
-        System.out.println("test1");
+        System.out.println("test2");
     }
 
     @Test
